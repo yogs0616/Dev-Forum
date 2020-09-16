0131e44febabbf940da39f4f39c0609fffa9b994
yogendra <yogendra.s.shetty@gmail.com>
2020-09-16 18:02:13 +0530

    setting up server

--- src/netcracker/developer/imp/SessionHandling.java
+++ src/netcracker/developer/imp/SessionHandling.java
@@ -3,47 +3,48 @@ package netcracker.developer.imp;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 /**
  * SessionHandling.java: This program Consists of session management Which can be called as functions to incorporate sessions and manage sessions.
- * @author lavp0616
  *
+ * @author lavp0616
  */
 public class SessionHandling {
 
-	/**This Method sessionForLoginGet is Called to check if whether a logged in user is trying to login again or a new user is trying to login.
-	 * If logged in user tries to login again without logout then it will show warning and retrieve the 
-	 * 
-	 * @param request
-	 * @param response
-	 * @param session
-	 * @return
-	 */
-	public static int sessionForLoginGet(HttpServletRequest request,HttpServletResponse response,HttpSession session){
-		/**
-		 * 1:admin
-		 * 2:member
-		 * 3:login
-		 */
-			if(session!=null){
-			String sessionId=(String)session.getAttribute("sessionId");
-				if(session.getId()!=sessionId){
-				return 3;
-				}else{
-						String user=(String) session.getAttribute("role");
-						if(user.equals("admin")){
-							return 1;
-						}else if(user.equals("member")){
-							return 2;
-						}
-						else{
-							return 3;
-						}
-					}
-				}else{
-					
-					return 3;
-				}
-		}
+    /**
+     * This Method sessionForLoginGet is Called to check if whether a logged in user is trying to login again or a new user is trying to login.
+     * If logged in user tries to login again without logout then it will show warning and retrieve the
+     *s
+     * @param request
+     * @param response
+     * @param session
+     * @return
+     */
+    public static int sessionForLoginGet(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        /**
+         * 1:admin
+         * 2:member
+         * 3:login
+         */
+        if (session != null) {
+            String sessionId = (String) session.getAttribute("sessionId");
+            if (!session.getId().equals(sessionId)) {
+                return 3;
+            } else {
+                String user = (String) session.getAttribute("role");
+                if (user.equals("admin")) {
+                    return 1;
+                } else if (user.equals("member")) {
+                    return 2;
+                } else {
+                    return 3;
+                }
+            }
+        } else {
+
+            return 3;
+        }
+    }
 }
 
 	
